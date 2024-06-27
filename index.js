b977f708e3be108faaaf70d65725fe78ace23f90
Isha3007 <ishitaramdasi3007@gmail.com>
2024-06-27 17:04:40 +0530

    added box shadows

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ for(var i=0;i<document.querySelectorAll(".piano-keys").length;i++)
         document.querySelectorAll(".piano-keys")[i].addEventListener("click", function ()
         {
             makesound(this.id)
+            animate(this.id);
         });
     }
 
@@ -12,4 +13,14 @@ function makesound(value)
     var audio = new Audio(x);
     audio.play();
  
+ }
+
+ function animate(val)
+ {
+    var active = document.getElementById(val);
+    active.classList.add("pressed");
+    setTimeout(function()
+{
+    active.classList.remove("pressed");
+}, 100);
  }
